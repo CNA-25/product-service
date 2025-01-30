15b7e11ef0d3d4dfcedbe36eb8c1951992a533ed
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2025-01-30 14:38:13 +0200

    Hade problem med att få något att visa på rahti. Testar.

--- src/server.js
+++ src/server.js
@@ -5,15 +5,18 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.json());  // Ensure JSON parsing works
+
 app.use(cors({
     origin: process.env.MODE === "development"
         ? "*"
-        : "https://product-service-git-cna-product-service.2.rahtiapp.fi/"
+        : "https://people.arcada.fi"
 }));
 
 app.get("/", (req, res) => {
-    console.log(req.myVar);
     res.send("<h1>Hello!!!</h1>");
 });
 
-app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
