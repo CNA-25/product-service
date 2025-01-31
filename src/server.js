2116dbb53262e45114d6df86ee2c612dacd9f3be
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2025-01-31 13:24:59 +0200

    Testar ny webhook

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
 
-console.log(`Node.js ${process.version}`)
+console.log(`Node.js ${process.version}`);
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ msg: "Rahti2 node 0.2" })
-})
+    res.json({ msg: "Rahti2 node 0.2" });
+});
 
 
 app.listen(PORT, () => {
     try {
-        console.log(`Running on http://localhost:${PORT}`)
+        console.log(`Running on http://localhost:${PORT}`);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-    
-})
+
+});
+
+// Testing new webhook
